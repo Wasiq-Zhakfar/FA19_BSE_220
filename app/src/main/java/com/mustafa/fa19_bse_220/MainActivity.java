58fb67b80ee93dff6d41833e5f4c5aeecea87c3a
Wasiq Zhakfar <wasiqzhakfar@gmail.com>
2022-04-12 14:23:14 +0500

    FA19-BSE-220

--- app/src/main/java/com/mustafa/fa19_bse_220/MainActivity.java
+++ app/src/main/java/com/mustafa/fa19_bse_220/MainActivity.java
@@ -24,13 +24,12 @@ public class MainActivity extends AppCompatActivity {
         addRecord.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });replaceFragment (new addRecord());
     }
 
-    private void replaceFragment(com.mustafa.fa19_bse_220.addRecord addRecord) {
-    }
+
 
 
 }
